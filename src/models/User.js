1abac9de4e6adf61426b4b7d7001930841f01431
Felipe Seabra <feliperoce@gmail.com>
2023-01-26 16:54:39 -0300

    feat: requirement 02

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+const UsarModuleSchema = (sequelize, Datatypes) => {
+  const UserModuleTable = sequelize.define(
+    'User',
+    {
+      id: {
+        type: Datatypes.INTEGER,
+        primaryKey: true,
+      },
+      displayName: Datatypes.STRING,
+      email: Datatypes.STRING,
+      password: Datatypes.STRING,
+      image: Datatypes.STRING,
+    },
+    {
+      timestamp: false,
+      tableName: "users",
+      underscored: true,
+    }
+  );
+
+  return UserModuleTable;
+};
+
+module.exports = UsarModuleSchema;
